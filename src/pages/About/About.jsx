1d6f5a23bf68482e90919ea630370f1e9a0180d2
dexie812 <mypc76239@gmail.com>
2024-08-26 06:15:46 -0400

    about description changed

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -44,19 +44,12 @@ const About = () => {
     </header>
 
     <section className="about-text">
-      <p>
-        I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media.
-        I enjoy
-        turning complex problems into simple, beautiful and intuitive designs.
-      </p>
-
-      <p>
-        My job is to build your website so that it is functional and user-friendly but at the same time attractive.
-        Moreover, I
-        add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring
-        across your
-        message and identity in the most creative way. I created web design for many famous brand companies.
-      </p>
+    <p>
+          I am a creative Web Developer from Dhaka, Bangladesh, working in web development for some years. I enjoy turning complex problems into simple, beautiful designs.
+        </p>
+        <p>
+          My job is to build your website so that it is functional and user-friendly but at the same time attractive.Morever, I add personal touch to your product and make sure that it is eye-catching and easy to use . My aim is to bring across your message and identity in the most creative way. I created website for many brand companies.
+        </p>
     </section>
 
 
